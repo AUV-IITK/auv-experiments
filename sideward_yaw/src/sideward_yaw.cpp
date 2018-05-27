26b476aaa1686d276d6e411f824e4dd52d34c677
Suryansh Agarwal <suryansh.a98@gmail.com>
2018-05-27 19:38:11 +0530

    introduced a variable to store the pwm values

--- sideward_yaw/src/sideward_yaw.cpp
+++ sideward_yaw/src/sideward_yaw.cpp
@@ -15,6 +15,10 @@ std_msgs::Int32 pwm_sidewardFront;
 std_msgs::Int32 pwm_sidewardBack;
 std_msgs::Int32 pwm_turn;
 
+int sidewardFront_PWM_data_ = 0;
+int sidewardBack_PWM_data_ = 0;
+int turn_PWM_data_ = 0;
+
 // ros::Publisher PWM_turn;
 ros::Publisher PWM_sidewardFront;
 ros::Publisher PWM_sidewardBack;
@@ -41,23 +45,40 @@ void turningOutputPWMMapping(float output) // to keep PWM values within a limit
     output_pwm = 255;
   if (output_pwm < -255)
     output_pwm = -255;
-  pwm_turn.data = output_pwm;
+  // pwm_turn.data = output_pwm;
+  turn_PWM_data_ = output_pwm;
 }
 
 void PWMsideward(std_msgs::Int32 msg)
 {
-  pwm_sidewardFront.data = msg.data;
-  pwm_sidewardBack.data = msg.data;
+  // pwm_sidewardFront.data = msg.data;
+  sidewardFront_PWM_data_ = msg.data;
+
+  // pwm_sidewardBack.data = msg.data;
+  sidewardBack_PWM_data_ = msg.data;
 }
-void publish(std_msgs::Int32 pwm)
+// void publish(std_msgs::Int32 pwm)
+// {
+//     pwm_sidewardFront.data = pwm_sidewardFront.data + pwm.data;
+//     pwm_sidewardBack.data = pwm_sidewardBack.data - pwm.data;
+// 
+//     PWM_sidewardFront.publish(pwm_sidewardFront);
+//     PWM_sidewardBack.publish(pwm_sidewardBack);
+// }
+
+void publish(int sidewardFront_PWM_data_, int sidewardBack_PWM_data_, int turn_PWM_data_)
 {
-    pwm_sidewardFront.data = pwm_sidewardFront.data + pwm.data;
-    pwm_sidewardBack.data = pwm_sidewardBack.data - pwm.data;
 
-    PWM_sidewardFront.publish(pwm_sidewardFront);
-    PWM_sidewardBack.publish(pwm_sidewardBack);
+	pwm_sidewardFront.data = sidewardFront_PWM_data_ + turn_PWM_data_;
+	pwm_sidewardBack.data = sidewardBack_PWM_data_ - turn_PWM_data_;
+
+	PWM_sidewardFront.publish(pwm_sidewardFront);
+	PWM_sidewardBack.publish(pwm_sidewardBack);
+
+	std::cout << "Data Published on SidewardFront thruster: " << pwm_sidewardFront.data << std::endl;
+	std::cout << "Data published on SidewardBack thruster: " << pwm_sidewardBack.data << std::endl;
+
 }
-
 void yawCb(std_msgs::Float64 msg)
 {
     double initial_loop_time = ros::Time::now().toNSec();
@@ -108,7 +129,8 @@ void yawCb(std_msgs::Float64 msg)
         if (error < band && error > -band)
         {
             std::cout << "ERROR: " << "1" << std::endl;
-            pwm_turn.data = 0;
+            // pwm_turn.data = 0;
+	    turn_PWM_data_ = 0;
         }
 
         double final_loop_time = ros::Time::now().toNSec();
@@ -123,24 +145,30 @@ void yawCb(std_msgs::Float64 msg)
         // PWM_turn.publish(pwm_turn);
 //        PWM_sidewardFront.publish(pwm_sidewardFront);
 //        PWM_sidewardBack.publish(pwm_sidewardBack);
-        publish(pwm_turn);
+//        publish(pwm_turn);
     }
 }
 
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "sideward_yaw");
-  ros::NodeHandle nh_;
+	ros::init(argc, argv, "sideward_yaw");
+	ros::NodeHandle nh_;
 
-  std::cout << "Sideward_Yaw initiated, p: " << " " << "i: " << " " << "d: " <<std::endl;
+	std::cout << "Sideward_Yaw initiated, p: " << " " << "i: " << " " << "d: " <<std::endl;
 
-  ros::Subscriber yaw = nh_.subscribe<std_msgs::Float64>("/varun/motion/yaw", 1000, &yawCb);
-  ros::Subscriber pwm_sideward = nh_.subscribe<std_msgs::Int32>("/pwm/sideward", 1000, &PWMsideward);
-  // PWM_turn = nh_.advertise<std_msgs::Int32>("/pwm/turn", 400);
-  PWM_sidewardFront = nh_.advertise<std_msgs::Int32>("/pwm/sidewardFront", 400);
-  PWM_sidewardBack = nh_.advertise<std_msgs::Int32>("/pwm/sidewardBack", 400);
+	ros::Subscriber yaw = nh_.subscribe<std_msgs::Float64>("/varun/motion/yaw", 1000, &yawCb);
+	ros::Subscriber pwm_sideward = nh_.subscribe<std_msgs::Int32>("/pwm/sideward", 1000, &PWMsideward);
+	PWM_sidewardFront = nh_.advertise<std_msgs::Int32>("/pwm/sidewardFront", 400);
+	PWM_sidewardBack = nh_.advertise<std_msgs::Int32>("/pwm/sidewardBack", 400);
+	
+	ros::Rate loop_rate(50);
 
-  ros::spin();
-  return 0;
+	while(ros::ok())
+	{
+		publish(sidewardFront_PWM_data_, sidewardBack_PWM_data_, turn_PWM_data_);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
 }
