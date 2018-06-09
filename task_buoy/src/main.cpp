63ce08ebc962b6fd7c3a9e06d907bf50bdd3a275
ksakash <akashsingh049811@gmail.com>
2018-06-09 23:58:15 +0530

    updated main

--- task_buoy/src/main.cpp
+++ task_buoy/src/main.cpp
@@ -6,25 +6,69 @@
 #include <string>
 #include "std_msgs/Float64MultiArray.h"
 #include <errorToPWM.h>
+#include <geometry_msgs/PointStamped.h>
 
 ErrorDescriptor angle("ANGLE");
-ErrorDescriptor x_coord("X_COORD");
-ErrorDescriptor y_coord("Y_COORD");
-ErrorDescriptor z_coord("Z_COORD");
+ErrorDescriptor x_coord("X_COORD"); // x_coord along the width of the screen
+ErrorDescriptor y_coord("Y_COORD"); // y_coord along the height
+ErrorDescriptor z_coord("Z_COORD"); // z_coord for distance
 
 int count_buoy = 0;
 int count_imu = 0;
 
-void buoyDataCallback(const std_msgs::Float64MultiArrayPtr &_msg) {
+double move_forward_duration = 0;
+double move_forward_start = 0;
+
+bool move_forward_signal = false;
+bool move_backward_signal = false;
+
+void buoyDataCallback(const geometry_msgs::PointStampedPtr &_msg) {
     if (count_buoy == 0) {
         x_coord.setReference(0);
         y_coord.setReference(0);
         z_coord.setReference(0);
         count_buoy++;
     }
-    x_coord.errorToPWM(_msg->data[1]);
-    y_coord.errorToPWM(_msg->data[2]);
-    z_coord.errorToPWM(_msg->data[3]);
+    if (_msg->point.x <= 30 && count_buoy == 1) {
+        move_forward_signal = true;
+        move_forward_start = ros::Time::now().toSec();
+        count_buoy++;
+        ROS_INFO("MOVING FORWARD WITH CONSTANT PWM NOW!!!!");
+    }
+
+    if (ros::Time::now().toSec() >= move_forward_start + move_forward_duration && count_buoy == 2) {
+        move_backward_signal = true;
+        move_forward_signal = false;
+        count_buoy++;
+        ROS_INFO("MOVING BACKWARD WITH CONSTANT PWM NOW!!!!");
+    }
+
+    if (_msg->point.x >= 30 && count_buoy == 3) {
+        move_backward_signal = false;
+        x_coord.setReference(0);
+        y_coord.setReference(0);
+        z_coord.setReference(50);
+        ROS_INFO("NOW MOVING A BACK LITTLE BIT!!!! ");
+    }
+
+    if (move_forward_signal) {
+        x_coord.errorToPWM(0);
+        y_coord.errorToPWM(0);
+        z_coord.errorToPWM(30);        
+    }
+    else if (move_backward_signal) {
+        x_coord.errorToPWM(0);
+        y_coord.errorToPWM(0);
+        z_coord.errorToPWM(-30);                
+    }
+    else {
+        // assuming point.y is for sideward position
+        // point.x for distance
+        // point.z for depth
+        x_coord.errorToPWM(_msg->point.y); 
+        y_coord.errorToPWM(_msg->point.z);
+        z_coord.errorToPWM(_msg->point.x);
+    }
 }
 
 void imuDataCallback(const std_msgs::Float64Ptr &_msg) {
@@ -37,8 +81,8 @@ void imuDataCallback(const std_msgs::Float64Ptr &_msg) {
 int main(int argc, char **argv) {
     ros::init(argc, argv, "task_buoy");
     ros::NodeHandle nh;
-    ros::Subscriber buoyDataListener = nh.subscribe("/varun/ip/buoy", 1000, &buoyDataCallback);
-    ros::Subscriber imuDataListener = nh.subscribe("/varun/imu", 1000, &imuDataCallback);
+    ros::Subscriber buoyDataListener = nh.subscribe("/threshold/center_coordinates", 1000, &buoyDataCallback);
+    ros::Subscriber imuDataListener = nh.subscribe("/varun/sensors/imu/yaw", 1000, &imuDataCallback);
     
     ros::Publisher frontSidewardPublisher = nh.advertise<std_msgs::Int32>("/pwm/sidewardFront", 1000);
     ros::Publisher backSidewardPublisher = nh.advertise<std_msgs::Int32>("/pwm/sidewardBack", 1000);
